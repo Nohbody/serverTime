6f18d452cad6585093c699a0ea6b72e1df585b80
Christopher Murphy <christmurphy@gmail.com>
2014-10-24 15:40:10 -0600

    added a user class

---
+++ src/user.java
@@ -0,0 +1,48 @@
+/**
+ * Created by newScanTron on 10/24/2014.
+ */
+public class user
+{
+
+    String email;
+    String password;
+    String name;
+
+    public void user(String name, String email, String password)
+    {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+}
